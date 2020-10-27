4be05ad5d08cc3969fcdb686772ebd8b11e9e912
dsnyder-ciat <73513037+dsnyder-ciat@users.noreply.github.com>
2020-10-26 21:12:45 -0700

    Update wk3-1b.c

--- wk3-1b.c
+++ wk3-1b.c
@@ -8,6 +8,7 @@ int main ()
   char text [] = "Hello World.";
   int letterCount = strlen(text);
 
+  // loops through each letter
   for (int i=0; i<letterCount; i++)
   {
     text [i] = toupper(text[i]);
