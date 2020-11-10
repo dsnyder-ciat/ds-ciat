bc172327df2462c32a98e1dced05e0914cd7aa1e
dsnyder-ciat <73513037+dsnyder-ciat@users.noreply.github.com>
2020-11-10 15:32:09 -0800

    Create pin.c

---
+++ Week 5/pin.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+// Function Prototypes:
+void enterPin();
+
+// Variables for all Functions to use:
+char pin[6];
+char pin2[6];
+int menu;
+int i;
+
+// Main Menu function
+int main()
+{
+  printf("\nMain Menu: \n");
+  printf("1. Enter Pin\n2. Exit\n\n");
+  scanf("%i", &menu);
+
+  switch(menu)
+  {
+    case 1:
+      enterPin();
+      break;
+
+    case 2:
+      printf("Goodbye!");
+
+    default:
+      printf("Invalid selection");
+      break;
+  }
+
+  return 0;
+}
+
+// Function to enter in a PIN
+void enterPin()
+{
+  printf("\nEnter your six-digit PIN: ");
+  scanf("%s", pin);
+
+  // Uses strlen to verify required length
+  // If it doesn't meet the length it calls enterPin() and starts over
+  if (strlen(pin) != 6 || pin[0] =='0')
+  {
+    printf("\nError : PIN must be 6 numbers, no letters, and cannot start with 0. \n");
+    enterPin();
+  }
+
+  printf("Enter your PIN again: ");
+  scanf("%s", pin2);
+
+  // Uses strlen to verify required length
+  // If it doesn't meet the length it calls enterPin() and starts over
+  if (strlen(pin2) != 6 || pin2[0] =='0')
+  {
+    printf("\nError : PIN must be 6 numbers, no letters, and cannot start with 0. \n");
+    enterPin();
+  }
+
+  for (int i = 0; i<6; i++)
+  {
+    if (pin[i] != pin2[i])
+    {
+      printf("\nYour PIN doesn't match! Try again!\n");
+      enterPin();
+    }
+  }
+
+  printf("\nPIN saved successfully.\nReturning to Menu\n");
+  main();
+
+}
